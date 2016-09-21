b2f199786ff88b607033b3e6eba8f5380db7b7d6
Lorenzo Pichilli <pichillilorenzo@gmail.com>
2016-09-21 17:26:58 +0200

    fix for #12 on windows

--- node/node_variables.py
+++ node/node_variables.py
@@ -11,8 +11,8 @@ NODE_JS_BINARIES_FOLDER_NAME = "node_binaries"
 NODE_JS_VERSION_URL_LIST_ONLINE = "https://nodejs.org/dist/index.json"
 NODE_JS_BINARIES_FOLDER = os.path.join(PACKAGE_PATH, NODE_JS_BINARIES_FOLDER_NAME)
 NODE_JS_BINARIES_FOLDER_PLATFORM = os.path.join(NODE_JS_BINARIES_FOLDER, sublime.platform() + "-" + PLATFORM_ARCHITECTURE)
-NODE_JS_PATH_EXECUTABLE = os.path.join(NODE_JS_BINARIES_FOLDER_PLATFORM, "node")
 os_switcher = {"osx": "darwin", "linux": "linux", "windows": "win"}
 NODE_JS_OS = os_switcher.get(sublime.platform())
 NODE_JS_ARCHITECTURE = "x64" if PLATFORM_ARCHITECTURE == "64bit" else "x86"
-NODE_JS_BINARY_NAME = "node"
+NODE_JS_BINARY_NAME = "node" if NODE_JS_OS != 'win' else "node.exe"
+NODE_JS_PATH_EXECUTABLE = os.path.join(NODE_JS_BINARIES_FOLDER_PLATFORM, NODE_JS_BINARY_NAME)
