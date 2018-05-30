1cb32f5d9bd54961ea1a358826e3ce5e31aff383
pichillilorenzo <pichillilorenzo@gmail.com>
2018-05-30 21:14:54 +0200

    added version 0.16.18

--- src/libs/global_vars.py
+++ src/libs/global_vars.py
@@ -1,6 +1,6 @@
 import os
 
-PLUGIN_VERSION = "0.16.17"
+PLUGIN_VERSION = "0.16.18"
 DEVELOPER_MODE = False
 
 PACKAGE_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
